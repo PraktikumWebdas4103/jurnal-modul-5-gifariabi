9723d1728e275ad8123f4e6ad98816e682ce7a8c
gifariabi <43516521+gifariabi@users.noreply.github.com>
2018-10-02 16:52:17 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,17 @@
+<form action=" " method="POST">
+	<input type="text" name="komen"><br>
+	<input type="submit" name="submit" value="input">
+</form>
 <?php
+	if (isset($_POST['submit'])) {
+		$komen = $_POST['komen'];
 
-?>
+		$k = strlen($komen);
+
+		if ($k < 5) {
+			echo "Jumlah kata kurang";
+		}
+		else{
+			echo "$k";
+		}
+	}
